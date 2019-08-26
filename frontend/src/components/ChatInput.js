fdca42112e34e49f4b0e691a28414c2d9402626e
kasrakhosravi <kasrakhosravi2@gmail.com>
2019-08-26 19:47:48 +0200

    refactor ChatInput

--- frontend/src/components/ChatInput.js
+++ frontend/src/components/ChatInput.js
@@ -1,34 +1,45 @@
+
+// global dependencies
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+
 class ChatInput extends Component {
-  static propTypes = {
-    onSubmitMessage: PropTypes.func.isRequired,
-  }
+
   state = {
     message: '',
-  }
+  };
+
+  onChange = (e) => {
+    this.setState({ message: e.target.value })
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmitMessage(this.state.message);
+    this.setState({ message: '' })
+  };
 
   render() {
     return (
       <form
         action=".."
-        onSubmit={e => {
-          e.preventDefault()
-          this.props.onSubmitMessage(this.state.message)
-          this.setState({ message: '' })
-        }}
+        onSubmit={this.onSubmit}
       >
         <input
           type="text"
           placeholder={'Enter message...'}
           value={this.state.message}
-          onChange={e => this.setState({ message: e.target.value })}
+          onChange={this.onChange}
         />
         <input type="submit" value={'Send'} />
       </form>
     )
   }
 }
+
+ChatInput.propsTypes = {
+  onSubmitMessage: PropTypes.func.isRequired,
+};
 
 export default ChatInput
